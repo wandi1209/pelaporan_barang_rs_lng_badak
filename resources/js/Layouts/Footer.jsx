0a00d912ed366a83d1d09e18d7b4036c4bc7a472
wandi1209 <wndi1209@gmail.com>
2023-12-14 12:44:10 +0800

    first commit

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "@inertiajs/react"
 export default function Footer(){
     return(
         <Link href={route('developer')} className="text-white bg-cyan-900/60 p-3 fixed lg:right-5 bottom-5 cursor-pointer">
-            <div>Web Developer = PKL SMK Negeri 1 2023</div>
+            <div>Web Developer = PKL SMK Negeri 1 Tahun 2023</div>
         </Link>
     )
 }
